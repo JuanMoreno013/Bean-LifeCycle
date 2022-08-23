18ae5512fd88942096dc29ec39dca8590369f2c7
Juan Moreno <jmoreno@griddynamics.com>
2022-08-23 09:53:24 -0500

    Initial commit

---
+++ src/test/java/com/example/BeanCycle/StudentCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BeanCycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
